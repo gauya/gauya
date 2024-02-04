fba71476aae424bc7a26cb782df8bdcb500b18c5
hwang chiyeong <hwang.chiyeong@gmail.com>
2024-02-04 12:02:44 +0900

    step4

--- src/step4.h
+++ src/step4.h
@@ -2,16 +2,18 @@
 #include <stdlib.h>
 #include <arduino.h>
 
+#ifndef __STEP4_H__
+#define __STEP4_H__
+
 #define LH(cdata) (((cdata)? HIGH: LOW))
 
 class byj {
   private:
     int _ports[4];
   public:
-    const int min_delay = 2;
-    const int def_delay = 2;
-    const double min_speed = 0.00001;
-
+    static const int min_delay = 2;
+    static constexpr double min_speed = 0.00001f;
+    
     void init(int *ps);
     void onestep(int dir);
     void mstep(int val);
@@ -25,61 +27,42 @@ class byj {
     void init();
     void go(double speed, uint16_t distance); // angle
     void stop() { onestep(0); };
+    operator int *() { return (int*)_ports; }
   /////////////////////////////////////////////////////////////  
     void test();
 };
 
 class step4_job : byj {
 private:
-  int delay; // speed
-  uint16_t delay_cnt; // scan period * delay_cnt, ... delay_cnt = delay; delay_cnt--
-  int dir;
+  int _delay; // check per 100 usec
+  uint16_t _delay_cnt; // scan period * delay_cnt, ... delay_cnt = delay; delay_cnt--
+  int _step_no; // 0~7 , -1 : default
 
 public:
-  int speed;
-  uint16_t distance; // distance--
+  int _dir; //-1, 0(stoped), 1
+  double _speed; // -1 ~ 1
+  uint16_t _distance; // distance--
 
 public:
   step4_job(int *ps);
   ~step4_job();
 
+  void go(double speed, uint16_t distance);
+  void seq_step();
 };
 
-struct byj_job {
-  byj *motor;
-  int dir;
-  int delay; // speed
-  uint16_t delay_cnt; // scan period * delay_cnt, ... delay_cnt = delay; delay_cnt--
-  uint16_t distance; // distance--
+#define MAX_STEP4_MOTOR 4
 
-  int isalive() { return (motor)? 1 : 0; };
-  void close() { motor = (byj*)0; }
-  void set(struct byj_job *ss);
-  void onestep();
-};
+void scan_step4();
 
-#define MAX_JOB 16
-#if 0
-struct byj_job jobs_table[MAX_JOB];
+#endif // __STEP4_H__
 
-int get_freetab(); // -1 fail
-int set_tab(struct cmd_step*); // -1 fail
-void init_tab();
-void scan_tab(); // timer interrupt
-#endif
+void step4_job::go(double speed, uint16_t distance) {
+  noInterrupts();
 
-class scanjobs {
-private:
-  static int _initialized;
-  static struct byj_job _cmds[MAX_JOB];
-  void init_tab();
-public:
-  scanjobs();
-  ~scanjobs();
-
-  int set_tab(struct byj_job*);
-  static void scan();
-};
-
-
-
+  _dir = (_speed < 0)? -1: (abs(speed) < min_speed)? 0 : 1;
+  _speed = abs(speed);
+  _distance = distance;
+  
+  interrupts();
+}
